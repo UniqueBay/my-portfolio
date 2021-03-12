d5ab94b56a68c653cf4dbdfd8b20626676cb85a1
Alabi Abayomi <abayomi15@gmail.com>
2021-03-12 04:41:10 +0100

    complete navbar content

---
+++ src/Components/NavBar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import avatar from "../Img/avatar.jpg";
+import {NavLink} from "react-router-dom";
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <nav className="nav">
+                <div className="profile">
+                    <img src={avatar} alt="abayomi" />
+                </div>
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <NavLink to="/" exact activeClassName="active">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about" exact activeClassName="active">
+                            About
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/projects" exact activeClassName="active">
+                            Projects
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/contact" exact activeClassName="active">
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+                <footer className="footer">
+                    <p>
+                        @2021 Abayomi Alabi 
+                    </p>
+                </footer>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
